cf33e06e9513b417af4fee72281f22d5ef0500b9
Vladimir Goriachev <vladinstein@gmail.com>
2023-08-08 00:34:42 -0400

    switched to collidepoint instead of coords

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -8,11 +8,14 @@ class Tic_Tac_Toe:
     # Add check_win method
 
 def draw_grid():
+    rects = []
     square_size = 200
-    for x in range(3):
-        for y in range(3):
+    for y in range(3):
+        for x in range(3): 
             rect = pygame.Rect(x * square_size, y * square_size, square_size, square_size)
+            rects.append(rect)
             pygame.draw.rect(screen, black, rect, 3)
+    return rects 
 
 pygame.init()
 
@@ -31,15 +34,14 @@ board = Tic_Tac_Toe()
 
 
 while True:
-    print(board.square)
-    draw_grid()
-    x = y = 0
+    tiles = draw_grid()
+    x = y = -1
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
         if event.type == pygame.MOUSEBUTTONDOWN:
             x, y = pygame.mouse.get_pos()
-    # First square 
-    if x > 0 and x < width / 3 and y > 0 and y < height/3 and board.square[0] == 0:
+    # First square
+    if tiles[0].collidepoint(x, y) and board.square[0] == 0:
         if move == True:
             # If it's a first player's move, draw a cross
             pygame.draw.line(screen, black, (width / 30, height / 30), (width * 9 / 30, height * 9 / 30), 9)
@@ -51,7 +53,7 @@ while True:
             board.make_move(0, 2)
         move = not move
     # Second square
-    if x > width / 3 and x < width * 2 / 3 and y > 0 and y < height / 3 and board.square[1] == 0:
+    if tiles[1].collidepoint(x, y) and board.square[1] == 0:
         if move == True:
             # If it's a first player's move, draw a cross
             pygame.draw.line(screen, black, (width * 11 / 30, height / 30), (width * 19 / 30, height * 9 / 30), 9)
@@ -63,7 +65,7 @@ while True:
             board.make_move(1, 2)
         move = not move
     # Third square
-    if x > width * 2 / 3 and x < width and y > 0 and y < height / 3 and board.square[2] == 0:
+    if tiles[2].collidepoint(x, y) and board.square[2] == 0:
         if move == True:
             # If it's a first player's move, draw a cross
             pygame.draw.line(screen, black, (width * 21 / 30, height / 30), (width * 29 / 30, height * 9 / 30), 9)
@@ -75,7 +77,7 @@ while True:
             board.make_move(2, 2)
         move = not move
     # Forth square
-    if x > 0 and x < width / 3 and y > height / 3 and y < height * 2 / 3 and board.square[3] == 0:
+    if tiles[3].collidepoint(x, y) and board.square[3] == 0:
         if move == True:
             # If it's a first player's move, draw a cross
             pygame.draw.line(screen, black, (width / 30, height * 11 / 30), (width * 9 / 30, height * 19 / 30), 9)
@@ -87,7 +89,7 @@ while True:
             board.make_move(3, 2)
         move = not move
     # Fifth square
-    if x > width / 3 and x < width * 2 / 3 and y > height / 3 and y < height * 2 / 3 and board.square[4] == 0:
+    if tiles[4].collidepoint(x, y) and board.square[4] == 0:
         if move == True:
             # If it's a first player's move, draw a cross
             pygame.draw.line(screen, black, (width * 11 / 30, height * 11 / 30), (width * 19 / 30, height * 19 / 30), 9)
@@ -99,7 +101,7 @@ while True:
             board.make_move(4, 2)
         move = not move  
     # Sixth square
-    if x > width * 2 / 3 and x < width and y > height / 3 and y < height * 2 / 3 and board.square[5] == 0:
+    if tiles[5].collidepoint(x, y) and board.square[5] == 0:
         if move == True:
             # If it's a first player's move, draw a cross
             pygame.draw.line(screen, black, (width * 21 / 30, height * 11 / 30), (width * 29 / 30, height * 19 / 30), 9)
@@ -111,7 +113,7 @@ while True:
             board.make_move(5, 2)
         move = not move 
     # Seventh square
-    if x > 0 and x < width / 3 and y > height * 2 / 3 and y < height and board.square[6] == 0:
+    if tiles[6].collidepoint(x, y) and board.square[6] == 0:
         if move == True:
             # If it's a first player's move, draw a cross
             pygame.draw.line(screen, black, (width / 30, height * 21 / 30), (width * 9 / 30, height * 29 / 30), 9)
@@ -123,7 +125,7 @@ while True:
             board.make_move(6, 2)
         move = not move
     # Eighth square
-    if x > width / 3 and x < width * 2 / 3 and y > height * 2 / 3 and y < height and board.square[7] == 0:
+    if tiles[7].collidepoint(x, y) and board.square[7] == 0:
         if move == True:
             # If it's a first player's move, draw a cross
             pygame.draw.line(screen, black, (width * 11 / 30, height * 21 / 30), (width * 19 / 30, height * 29 / 30), 9)
@@ -135,7 +137,7 @@ while True:
             board.make_move(7, 2)
         move = not move
     # Ninth square
-    if x > width * 2 / 3 and x < width and y > height * 2 / 3 and y < height and board.square[8] == 0:
+    if tiles[8].collidepoint(x, y) and board.square[8] == 0:
         if move == True:
             # If it's a first player's move, draw a cross
             pygame.draw.line(screen, black, (width * 21 / 30, height * 21 / 30), (width * 29 / 30, height * 29 / 30), 9)
