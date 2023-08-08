985afe51f3be8a182a67e3a2cfb595bcdff0a497
Vladimir Goriachev <vladinstein@gmail.com>
2023-08-07 23:05:06 -0400

    adding crosses and using a board class

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,16 +1,10 @@
 import sys, pygame
 
 class Tic_Tac_Toe:
-    a = [0] * 9
-    A1 = A2 = 'O'
-    #def check_win(self):
-    #    if self.a1 + self.a2 + self.a3 == 3:
-    #        return True
-        
-    def make_move(self, field, value):
-        self.a[field] = value
+    square = [0] * 9
 
-game = Tic_Tac_Toe()
+    def make_move(self, number, value):
+        self.square[number] = value
 
 def draw_grid():
     square_size = 200
@@ -20,7 +14,7 @@ def draw_grid():
             pygame.draw.rect(screen, black, rect, 3)
 
 #game.make_move(3, 1)
-#print(game.a)
+#print(board.square)
 #if game.check_win():
 #    print('Victory!')
 
@@ -36,8 +30,12 @@ screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 
 screen.fill(white)
+move = True
+board = Tic_Tac_Toe()
+
 
 while True:
+    print(board.square)
     draw_grid()
     x = y = 0
     for event in pygame.event.get():
@@ -45,32 +43,77 @@ while True:
         if event.type == pygame.MOUSEBUTTONDOWN:
             x, y = pygame.mouse.get_pos()
     # First square 
-    if x > 0 and x < width / 3 and y > 0 and y < height/3:
-        pygame.draw.circle(screen, black, (width / 6, width / 6), 85, 6)
+    if x > 0 and x < width / 3 and y > 0 and y < height/3 and board.square[0] == 0:
+        if move == True:
+            # If it's a first player's move, draw a cross
+            pygame.draw.line(screen, black, (width / 30, height / 30), (width * 9 / 30, height * 9 / 30), 9)
+            pygame.draw.line(screen, black, (width / 30, height * 9 / 30), (width * 9 / 30, height / 30), 9)
+            board.make_move(0, 1)
+        if move == False:
+            # Otherwise draw a circle
+            pygame.draw.circle(screen, black, (width / 6, height / 6), 85, 6)
+            board.make_move(0, 2)
+        move = not move
     # Second square
-    if x > width / 3 and x < width * 2 / 3 and y > 0 and y < height / 3:
-        pygame.draw.circle(screen, black, (width / 2, width / 6), 85, 6)
+    if x > width / 3 and x < width * 2 / 3 and y > 0 and y < height / 3 and board.square[1] == 0:
+        if move == True:
+            # If it's a first player's move, draw a cross
+            pygame.draw.line(screen, black, (width * 11 / 30, height / 30), (width * 19 / 30, height * 9 / 30), 9)
+            pygame.draw.line(screen, black, (width * 11 / 30, height * 9 / 30), (width * 19 / 30, height / 30), 9)
+            board.make_move(1, 1)
+        if move == False:
+            # Otherwise draw a circle
+            pygame.draw.circle(screen, black, (width / 2, height / 6), 85, 6)
+            board.make_move(1, 2)
+        move = not move
     # Third square
-    if x > width * 2 / 3 and x < width and y > 0 and y < height / 3:
-        pygame.draw.circle(screen, black, (width * 5 / 6, width / 6), 85, 6)
+    if x > width * 2 / 3 and x < width and y > 0 and y < height / 3 and board.square[2] == 0:
+        if move == True:
+            # If it's a first player's move, draw a cross
+            pygame.draw.line(screen, black, (width * 21 / 30, height / 30), (width * 29 / 30, height * 9 / 30), 9)
+            pygame.draw.line(screen, black, (width * 21 / 30, height * 9 / 30), (width * 29 / 30, height / 30), 9)
+            board.make_move(2, 1)
+        if move == False:
+            # Otherwise draw a circle
+            pygame.draw.circle(screen, black, (width * 5 / 6, height / 6), 85, 6)
+            board.make_move(2, 2)
+        move = not move
     # Forth square
-    if x > 0 and x < width / 3 and y > height / 3 and y < height * 2 / 3:
-        pygame.draw.circle(screen, black, (width / 6, width / 2), 85, 6)
+    if x > 0 and x < width / 3 and y > height / 3 and y < height * 2 / 3 and board.square[3] == 0:
+        if move == True:
+            # If it's a first player's move, draw a cross
+            pygame.draw.line(screen, black, (width / 30, height * 11 / 30), (width * 9 / 30, height * 19 / 30), 9)
+            pygame.draw.line(screen, black, (width / 30, height * 19 / 30), (width * 9 / 30, height * 11 / 30), 9)
+            board.make_move(3, 1)
+        if move == False:
+            # Otherwise draw a circle
+            pygame.draw.circle(screen, black, (width / 6, height / 2), 85, 6)
+            board.make_move(3, 2)
+        move = not move
     # Fifth square
-    if x > width / 3 and x < width * 2 / 3 and y > height / 3 and y < height * 2 / 3:
-        pygame.draw.circle(screen, black, (width / 2, width / 2), 85, 6)
+    if x > width / 3 and x < width * 2 / 3 and y > height / 3 and y < height * 2 / 3 and board.square[4] == 0:
+        if move == True:
+            # If it's a first player's move, draw a cross
+            pygame.draw.line(screen, black, (width * 11 / 30, height * 11 / 30), (width * 19 / 30, height * 19 / 30), 9)
+            pygame.draw.line(screen, black, (width * 11 / 30, height * 19 / 30), (width * 19 / 30, height * 11 / 30), 9)
+            board.make_move(4, 1)
+        if move == False:
+            # Otherwise draw a circle
+            pygame.draw.circle(screen, black, (width / 2, height / 2), 85, 6)
+            board.make_move(4, 2)
+        move = not move  
     # Sixth square
     if x > width * 2 / 3 and x < width and y > height / 3 and y < height * 2 / 3:
-        pygame.draw.circle(screen, black, (width * 5 / 6, width / 2), 85, 6)
+        pygame.draw.circle(screen, black, (width * 5 / 6, height / 2), 85, 6)
     # Seventh square
     if x > 0 and x < width / 3 and y > height * 2 / 3 and y < height:
-        pygame.draw.circle(screen, black, (width / 6, width * 5 / 6), 85, 6)
+        pygame.draw.circle(screen, black, (width / 6, height * 5 / 6), 85, 6)
     # Eighth square
     if x > width / 3 and x < width * 2 / 3 and y > height * 2 / 3 and y < height:
-        pygame.draw.circle(screen, black, (width / 2, width * 5 / 6), 85, 6)
+        pygame.draw.circle(screen, black, (width / 2, height * 5 / 6), 85, 6)
     # Ninth square
     if x > width * 2 / 3 and x < width and y > height * 2 / 3 and y < height:
-        pygame.draw.circle(screen, black, (width * 5 / 6, width * 5 / 6), 85, 6)
+        pygame.draw.circle(screen, black, (width * 5 / 6, height * 5 / 6), 85, 6)
 
     pygame.display.flip()
 
